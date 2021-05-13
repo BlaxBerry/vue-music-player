6f753e37ac0f790c39bfee357ba792e77b436127
Chen <chenjiaxu333@gmail.com>
2021-05-13 21:24:41 +0900

    Component Swipe

---
+++ src/components/Swipe/Swipe.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="swiper">
+    <el-carousel indicator-position="outside" type="" class="swipe_normal">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+    <el-carousel indicator-position="outside" type="card" class="swipe_card">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3>{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+@import "../../assets/style/Swipe/Swipe.less";
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="home">
+    <Swipe />
     home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+//Top Nav Bar
+import Swipe from "@/components/Swipe/Swipe.vue";
 
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    Swipe,
+  },
 };
 </script>
+
+<style lang="less" scoped>
+#home {
+  padding-top: 1rem;
+  padding-left: 7rem + 2rem;
+  padding-right: 1rem;
+}
+</style>
