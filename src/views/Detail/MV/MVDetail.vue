4521c37df6b6b8044be49e757edd3216bee5e3cd
BlaxBerry <chenjiaxu333@gmail.com>
2021-08-20 23:05:00 +0900

    MV Detail recommend list

--- src/views/Detail/MV/MVDetail.vue
+++ src/views/Detail/MV/MVDetail.vue
@@ -1,15 +1,25 @@
 <template>
   <v-row>
     <!-- left main -->
-    <v-col cols="12" md="9">
+    <v-col cols="12" md="9" class="pt-4">
       <video :src="MVURL" controls></video>
 
       <!-- recommend bottom -->
-      <div class="d-md-none"></div>
+      <v-row class="d-md-none d-flex align-center pa-2">
+        <v-col cols="6" class="pa-1" v-for="item in MVRecommendList" :key="item.id">
+          <RecommendList :item="item" />
+        </v-col>
+      </v-row>
     </v-col>
 
     <!-- right recommend list -->
-    <v-col class="d-none d-md-flex" md="3">2</v-col>
+    <v-col class="d-none d-md-flex pl-0 flex-column align-center" md="3">
+      <RecommendList
+        v-for="item in MVRecommendList"
+        :key="item.id"
+        :item="item"
+      />
+    </v-col>
   </v-row>
 </template>
 
@@ -17,8 +27,14 @@
 // api
 import { GetMVURL } from "@/api/mvURL.js";
 import { GetMVRecommend } from "@/api/mvRecommend.js";
+// components
+import RecommendList from "./RecommendList.vue";
 
 export default {
+  components: {
+    RecommendList,
+  },
+
   data() {
     return {
       MVURL: "",
---
+++ src/views/Detail/MV/RecommendList.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-card
+    class="d-flex align-center my-md-2 pa-0"
+    elevation="6"
+    @click="click(item.id)"
+  >
+    <!-- lg screen samll img -->
+    <v-img
+      class="d-none d-lg-flex mr-2 align-end red--text text--lighten-2"
+      :src="item.cover"
+      height="100"
+      max-width="150"
+    >
+      <div class="title d-flex align-center text-body-2 pl-2">
+        <!-- playCount -->
+        <v-icon small color="red lighten-2" class="mr-1">
+          mdi-play-circle-outline
+        </v-icon>
+        {{ item.playCount }}
+      </div>
+    </v-img>
+
+    <!-- md screen big template img -->
+    <v-img
+      class="d-md-flex d-lg-none white--text align-end"
+      :src="item.cover"
+      height="150"
+    >
+      <div class="title pa-2">
+        <v-card-subtitle class="pa-0 text-body-1 font-weight-black">
+          {{ item.name }}
+        </v-card-subtitle>
+        <v-card-subtitle
+          class="pa-0 pt-01 text-body-2 red--text text--lighten-2"
+        >
+          {{ item.artistName }}
+        </v-card-subtitle>
+      </div>
+    </v-img>
+
+    <!-- lg screen info desc -->
+    <div class="pa-0 d-none d-lg-block text-break">
+      <p class="text-subtitle-2 font-weight-bold ma-0 py-1">
+        {{ item.name }}
+      </p>
+      <p
+        v-for="artist in item.artists"
+        :key="artist.id"
+        class="text-caption ma-0 ml-1"
+      >
+        {{ artist.name }}
+        <!-- <small
+            v-if="artist.transNames"
+            class="font-weight-light"
+          >
+            ({{ artist.transNames[0] }})
+          </small> -->
+      </p>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+  methods: {
+    click(id) {
+      this.$router.push("/detail/mv?id=" + id);
+      this.$router.go(0);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-card {
+  width: 100%;
+  .title {
+    background-color: rgba(0, 0, 0, 0.5) !important;
+    min-height: 30px !important;
+  }
+  .v-img {
+    width: 100% !important;
+  }
+}
+</style>
